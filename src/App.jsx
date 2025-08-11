2e4bbe9da645720d528e9ced1fa20f71cdfdecc1
salehchy46 <salehchyctg@gmail.com>
2025-08-12 01:03:19 +0600

    React Project for bristo is ready to work on

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+      <div className="badge badge-outline badge-primary">Primary</div>
+      <div className="badge badge-outline badge-secondary">Secondary</div>
+      <div className="badge badge-outline badge-accent">Accent</div>
+      <div className="badge badge-outline badge-info">Info</div>
+      <div className="badge badge-outline badge-success">Success</div>
+      <div className="badge badge-outline badge-warning">Warning</div>
+      <div className="badge badge-outline badge-error">Error</div>
+    </>
+  )
+}
+
+export default App
