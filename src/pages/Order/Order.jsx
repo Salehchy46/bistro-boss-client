b43f4888ef3fcb3e8074dad1f49b8ace312226f7
salehchy46 <salehchyctg@gmail.com>
2025-08-19 16:49:05 +0600

    REeact tab is used

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -3,11 +3,19 @@ import orderCover from '../../assets/shop/banner2.jpg';
 import Cover from '../../components/Cover';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../hooks/useMenu';
 
 
 const Order = () => {
 
     const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+     //filtered menu items
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
 
     return (
         <div>
@@ -18,9 +26,12 @@ const Order = () => {
                     <Tab>Pizza</Tab>
                     <Tab>Dessert</Tab>
                     <Tab>Soup</Tab>
-                    <Tab>Salad</Tab>
-                    <Tab>Pizza</Tab>
+                    <Tab>Drinks</Tab>
+                    <Tab></Tab>
                 </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
             </Tabs>
